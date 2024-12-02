834f645bc721b0c87511a107cab7438df941a51b
墨青 <blackcyan07@outlook.com>
2024-12-02 23:58:48 +0800

    Revert "v1.1.2 添加了抓取错误信息抛出异常功能，提升使用体验"
    This reverts commit fcdd4027dc0a79d079225df727941a34586c425b.

--- script.py
+++ script.py
@@ -5,11 +5,10 @@ import os
 
 from DrissionPage import ChromiumPage
 from DrissionPage.common import Actions
-from DrissionPage.errors import *
 
 _author = '墨青BlackCyan'
 _name = 'Ulearning自动答题脚本'
-_version = '1.1.2'
+_version = '1.1.1'
 menu = [
     '1.自动答题',
     '2.反馈',
@@ -29,18 +28,8 @@ while True:
         os.system('cls')
         # 输入url
         url = input('将测验界面的Url粘贴至此：')
-        try:
-            a = re.findall('\d+', url)
-        except Exception:
-            print('Url错误, 可能是你粘贴了错误的Url\n按任意键继续...')
-            msvcrt.getch()
-            continue
-        try:
-            xhr_url = f'https://homeworkapi.ulearning.cn/quiz/homework/stu/questions?homeworkId={a[1]}&ocId={a[0]}&showAnswer=true'
-        except Exception:
-            print('Url错误, 可能是你粘贴了错误的Url\n按任意键继续...')
-            msvcrt.getch()
-            continue
+        a = re.findall('\d+', url)
+        xhr_url = f'https://homeworkapi.ulearning.cn/quiz/homework/stu/questions?homeworkId={a[1]}&ocId={a[0]}&showAnswer=true'
         # 修改url获取请求
         get_url = xhr_url
         # 用户自行输入authorization
@@ -55,12 +44,7 @@ while True:
         xhr_response = requests.get(url=xhr_url, headers=xhr_headers)
         # 储存JSON
         xhr_json = xhr_response.json()
-        try:
-            xhr_json['result']
-        except KeyError:
-            print('读取json键值对错误\n可能是你粘贴了错误的Url或Authorization?\n按任意键继续...')
-            msvcrt.getch()
-            continue
+
         # 爬取题目答案
         correctAnswer = {}
         ID = 0
@@ -86,12 +70,7 @@ while True:
                     answer = 4
                 elif correctAnswer[ID][0] == 'E':
                     answer = 5
-                try:
-                    ac.click(f'xpath://*[@id="app"]/div/div[1]/div[2]/div/div[1]/div/div/ul/li[{ID+1}]/div[2]/ul/li[{answer}]/div/label/span[1]/input')
-                except ElementNotFoundError:
-                    print(f'line: 75 未找到元素 xpath://*[@id="app"]/div/div[1]/div[2]/div/div[1]/div/div/ul/li[{ID+1}]/div[2]/ul/li[{answer}]/div/label/span[1]/input, 可能是源代码出现问题\n按任意键继续...')
-                    msvcrt.getch()
-                    continue
+                ac.click(f'xpath://*[@id="app"]/div/div[1]/div[2]/div/div[1]/div/div/ul/li[{ID+1}]/div[2]/ul/li[{answer}]/div/label/span[1]/input')
             elif len(correctAnswer[ID]) != 1:
                 for length in range(len(correctAnswer[ID])):
                     if correctAnswer[ID][length-1] == 'A':    answer = 1
@@ -99,23 +78,13 @@ while True:
                     elif correctAnswer[ID][length-1] == 'C':   answer = 3
                     elif correctAnswer[ID][length-1] == 'D':   answer = 4
                     elif correctAnswer[ID][length-1] == 'E':   answer = 5
-                    try:
-                        ac.click(f'xpath://*[@id="app"]/div/div[1]/div[2]/div/div[1]/div/div/ul/li[{ID+1}]/div[2]/ul/li[{answer}]/div/label/span[1]/input')
-                    except ElementNotFoundError:
-                        print(f'line: 86 未找到元素 xpath://*[@id="app"]/div/div[1]/div[2]/div/div[1]/div/div/ul/li[{ID + 1}]/div[2]/ul/li[{answer}]/div/label/span[1]/input, 可能是源代码出现问题\n按任意键继续...')
-                        msvcrt.getch()
-                        continue
+                    ac.click(f'xpath://*[@id="app"]/div/div[1]/div[2]/div/div[1]/div/div/ul/li[{ID+1}]/div[2]/ul/li[{answer}]/div/label/span[1]/input')
             elif len(correctAnswer[ID]) == 1 and (correctAnswer[ID][0] == 'true' or correctAnswer[ID][0] == 'false'):
                 if correctAnswer[ID][0] == 'true':
                     answer = 1
                 elif correctAnswer[ID][0] == 'false':
                     answer = 2
-                try:
-                    ac.click(f'xpath://*[@id="app"]/div/div[1]/div[2]/div/div[1]/div/div/ul/li[{ID+1}]/div[2]/div[2]/label[{answer}]/span[1]/input')
-                except ElementNotFoundError:
-                    print(f'line: 95 未找到元素 xpath://*[@id="app"]/div/div[1]/div[2]/div/div[1]/div/div/ul/li[{ID + 1}]/div[2]/ul/li[{answer}]/div/label/span[1]/input, 可能是源代码出现问题\n按任意键继续...')
-                    msvcrt.getch()
-                    continue
+                ac.click(f'xpath://*[@id="app"]/div/div[1]/div[2]/div/div[1]/div/div/ul/li[{ID+1}]/div[2]/div[2]/label[{answer}]/span[1]/input')
         print('自动答题完成！按任意键继续')
         msvcrt.getch()
         continue
